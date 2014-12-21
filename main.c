46f9a1d77017bbf6dae7b01f831c71475b8e5f59
mhl <mhl.mieczkowski@sent.com>
2014-12-21 21:55:53 +0100

    mythread_yield tests

--- main.c
+++ main.c
@@ -5,34 +5,34 @@
 #include "timer.h"
 #include "fifo.h"
 
-
+unsigned int counters[3];
 static void thread_function1(void *args) {
-        int i= 0;
-        for(i=0;i<5;i++) {
-                fprintf(stdout,"%d\n", i);
-                sleep(1);
+        int i;
+        for(i=0;i<100;i++) {
+                counters[0]++;
+                mythread_yield();
         }
         mythread_kill(1002);
         mythread_exit();
 }
 static void thread_function2(void *args) {
-        int x = 20000;
         for(;;) {
-                fprintf(stdout,"%d\n", x++);
-                sleep(1);
-                if(x==20015) 
-                        return;
+                counters[1]++;
+        }
+}
+static void thread_function3(void *args) {
+        for(;;) {
+                counters[2]++;
         }
 }
 
 int main() {
-        //This is for initial context
         sched_init();
         mythread_start(thread_function1, NULL);
         mythread_start(thread_function2, NULL);
-        timer_setup(sig_timer_handler, 250000);
-        schedule();
-        while(1);
+        mythread_start(thread_function3, NULL);
+        timer_setup(sig_timer_handler, 25000);
+        while(1) ;
         return 0;
 
 }
